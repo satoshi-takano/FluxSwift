b02972d67221e7b0b5cb568a9fb8ac50b0b00146
Satoshi Takano <satoxi.tacano@gmail.com>
2017-10-18 18:12:19 +0900

    Initial Commit

---
+++ FluxOnSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FluxOnSwift
+//
+//  Created by 高野 智史 on 2017/10/18.
+//  Copyright © 2017年 freee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
